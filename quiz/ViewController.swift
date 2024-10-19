c2f4a6fcab10f0755b5f47598939acbd67151e1c
sujal salgaonkar <sujalsalgaonkarbusiness@gmail.com>
2024-10-19 22:08:39 +0530

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by SUJAL SALGAONKAR on 19/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ quizTests/quizTests.swift
@@ -0,0 +1,17 @@
+//
+//  quizTests.swift
+//  quizTests
+//
+//  Created by SUJAL SALGAONKAR on 19/10/24.
+//
+
+import Testing
+@testable import quiz
+
+struct quizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
